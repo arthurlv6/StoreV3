86eafbaf159c15f63289a771779faf3c759f4344
Arthur Lu <arthur.lv6@gmail.com>
2020-10-19 17:43:52 +1300

    Master secret added.

--- API/Store/Store.Api/Controllers/JPushController.cs
+++ API/Store/Store.Api/Controllers/JPushController.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Store.Api.Models;
 using System;
-using System.Buffers.Text;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Store.Api.Controllers
 {
@@ -20,17 +16,17 @@ namespace Store.Api.Controllers
 
         public string JPushEndPoint { get; private set; }
         public string AppKey { get; private set; }
+        public string  MasterSecret { get; private set; }
         public JPushController(UserManager<ApplicationUser> userManager)
         {
             JPushEndPoint = "https://api.jpush.cn/v3/push";
             AppKey = "";
+            MasterSecret = "";
             _userManager = userManager;
         }
         protected string GetToken()
         {
-            var id = User.Identity.Name;
-            var user = _userManager.FindByEmailAsync(id).Result;
-            byte[] bytes = Encoding.ASCII.GetBytes($"{AppKey}:{user.JPushId}");
+            byte[] bytes = Encoding.ASCII.GetBytes($"{AppKey}:{MasterSecret}");
             return Convert.ToBase64String(bytes);
         }
         protected HttpClient AddHeader(HttpClient client)
